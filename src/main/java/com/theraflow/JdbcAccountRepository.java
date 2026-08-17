59105d4d47641c830a55b9d1ba9ff4e689d35d8e
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-08-17 15:16:26 +0200

    remove datasource configuration from application.yaml

--- src/main/java/com/theraflow/JdbcAccountRepository.java
+++ src/main/java/com/theraflow/JdbcAccountRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.support.TransactionTemplate;
 
