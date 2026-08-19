163a01814fe091366b289482cd30714b5b98f149
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-08-19 07:57:23 +0200

    Add AccountController

---
+++ src/main/java/com/theraflow/controller/AccountController.java
@@ -0,0 +1,33 @@
+package com.theraflow.controller;
+
+import com.theraflow.AccountService;
+import com.theraflow.model.AccountRequest;
+import com.theraflow.model.AccountResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> register(
+            @Valid @RequestBody AccountRequest request
+    ) {
+        AccountResponse response = accountService.createAccount(request);
+        URI location = URI.create("/api/v1/accounts/" + response.id());
+
+        return ResponseEntity.created(location)
+                .body(response);
+    }
+}
--- src/main/java/com/theraflow/model/AccountRequest.java
+++ src/main/java/com/theraflow/model/AccountRequest.java
@@ -1,9 +1,11 @@
 package com.theraflow.model;
 
 import com.theraflow.AccountType;
+import jakarta.validation.constraints.Email;
 import org.jspecify.annotations.NonNull;
 
 public record AccountRequest(
+        @Email
         @NonNull String email,
         @NonNull String rawPassword,
         @NonNull AccountType type
