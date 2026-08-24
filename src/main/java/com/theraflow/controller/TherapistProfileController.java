42b05ec528a8eddd4ec4d144c7fdfe6d9c0e4071
Ivan Voloshyn <ivan.voloshyn.ph@gmail.com>
2026-08-24 17:30:27 +0200

    Add TherapistProfileController create method

--- src/main/java/com/theraflow/controller/TherapistProfileController.java
+++ src/main/java/com/theraflow/controller/TherapistProfileController.java
@@ -1,12 +1,19 @@
 package com.theraflow.controller;
 
+import com.theraflow.dto.TherapistRequest;
 import com.theraflow.dto.TherapistResponse;
 import com.theraflow.service.TherapistService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,10 +22,18 @@ public class TherapistProfileController {
 
     private final TherapistService therapistService;
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<TherapistResponse> createTherapistProfile(
+            @Valid @RequestBody TherapistRequest request
     ) {
+        TherapistResponse therapist = therapistService.createTherapistProfile(request);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(therapist.id())
+                .toUri();
 
+        return ResponseEntity.created(location).body(therapist);
     }
 
 }
